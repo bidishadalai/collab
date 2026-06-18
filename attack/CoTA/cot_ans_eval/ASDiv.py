64585ae5b8f5aee509f309077f3575aedd50d772
bidisha <108706139+bidishadalai@users.noreply.github.com>
2026-06-18 06:19:53 -0400

    Update ASDiv.py

--- attack/CoTA/cot_ans_eval/ASDiv.py
+++ attack/CoTA/cot_ans_eval/ASDiv.py
@@ -4,7 +4,7 @@ from collections import Counter
 from math import isclose
 
 def test_answer(pred_str, ans_str, factor):
-    pattern = '\d*\.?\d+'
+    pattern = r'\d*\.?\d+'
     pred = re.findall(pattern, pred_str)
     if (len(pred) >= 1):
         # print(pred_str)
@@ -37,7 +37,7 @@ def find_step(doc, pool):
         return False, None
 
     # The number just in front of the step must appear in other reasoning steps
-    pattern = '\d*\.?\d+'
+    pattern = r'\d*\.?\d+'
     numbers = re.findall(pattern, doc)
     numbers_unique = []
     for number in numbers:
